04ab5c958559c7a05f474ca460cfcacf6476cdf1
prof.juanpablo <prof.juanpablo@gmail.com>
2021-10-28 07:07:34 -0300

    v.0.0.1 - ProEventos

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController(ILogger<EventoController> logger)
+        {
+        
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                Tema = "Encontro Turma .NET",
+                Local = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento= DateTime.Now.AddDays(2).ToString()
+            };
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Agora tem o metodo POST";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Agora tem o metodo PUT com o id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Agora tem o metodo Delete com o id = {id}";
+        }
+    }
+}
