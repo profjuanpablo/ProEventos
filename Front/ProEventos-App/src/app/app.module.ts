12596feafd1ab088ef9a263d7acbe0660b92c6db
prof.juanpablo <prof.juanpablo@gmail.com>
2021-10-31 19:09:34 -0300

    Instalação do Bootstrap

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,30 @@
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
