f32a2e797858fb1c20b569416dfbc6955c86e5a5
prof.juanpablo <prof.juanpablo@gmail.com>
2021-10-31 23:47:43 -0300

    filtro funcional

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any=[];
+  public eventosFiltrados: any = [];
+
+  marginImg: number=2;
+  widthImg: number=150;
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+   }
+
+   public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista): this.eventos;
+   }
+
+    filtrarEventos(filtrarPor: string): any {
+        filtrarPor= filtrarPor.toLocaleLowerCase();
+        return this.eventos.filter(
+          // (evento: any) => evento.tema.toLocaleLowerCase().indexof(filtroPor) !== -1
+          //  (evento:{tema: string;local:string})  => evento.tema.toLocaleLowerCase().indexOf(filtroPor) !== -1);
+
+        (evento:{tema: string;local:string;lote:string})  => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+       evento.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+
+        );
+    }
 
   constructor(private htpp: HttpClient) { }
 
@@ -16,9 +45,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
-  public getEventos(): void {
+    alterarImagem() {
+        this.mostrarImagem=!this.mostrarImagem;
+
+    }
+
+    public getEventos(): void {
     this.htpp.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error));
 
 
